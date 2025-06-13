0b9f67599c374bc31c788fb362c04a697d133f85
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2025-06-13 20:29:48 +0530

    Hero section done

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,5 @@
 import Button from "../components/Button";
+import HeroExperience from "../components/HeroModels/HeroExperience";
 import { words } from "../constants";
 
 const Hero = () => {
@@ -46,7 +47,13 @@ const Hero = () => {
             />
           </div>
         </header>
+
         {/*RIGHT Hero content */}
+        <figure>
+          <div className="hero-3d-layout border-red-200 border-2">
+            <HeroExperience />
+          </div>
+        </figure>
       </div>
     </section>
   );
