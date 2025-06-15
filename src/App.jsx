7eefdc53274376b72da4e3c7f8b17334ce7c6d68
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2025-06-15 12:32:52 +0530

    Timeline

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import LogoSection from "./components/LogoSection";
 import Navbar from "./components/Navbar";
+import ExperienceSection from "./sections/ExperienceSection";
 import FeatureCards from "./sections/FeatureCards";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
@@ -12,6 +13,7 @@ const App = () => {
       <ShowcaseSection />
       <LogoSection />
       <FeatureCards />
+      <ExperienceSection />
     </>
   );
 };
