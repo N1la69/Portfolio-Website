f574a7e113c3d450bb5a946b23cc319134fa68ff
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2025-06-15 10:58:51 +0530

    Feature cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import LogoSection from "./components/LogoSection";
 import Navbar from "./components/Navbar";
+import FeatureCards from "./sections/FeatureCards";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 
@@ -8,6 +10,8 @@ const App = () => {
       <Navbar />
       <Hero />
       <ShowcaseSection />
+      <LogoSection />
+      <FeatureCards />
     </>
   );
 };
